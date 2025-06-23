65f0ce13b1c2cc23d87eec5abe17a27decd5a8a0
akonoroshi <akr.oedo@gmail.com>
2025-06-23 16:58:00 -0700

    Utility-based retrieval

--- README.md
+++ README.md
@@ -14,15 +14,34 @@ or
 conda env create -f environment.yml
 ```
 
-Then, run the streamlit app
+We also need `poppler` to convert pdf to images. Poppler is very easy to install by following the instructions [on their website](https://poppler.freedesktop.org/). Or choose one of the following option:
+
+### MacOS with homebrew
 
 ```bash
-streamlit run app.py
+brew install poppler
 ```
+
+### Debian/Ubuntu
+
+```bash
+sudo apt-get install -y poppler-utils
+```
+
+## Enabling LLMs
+
+You can use your local LLMs or external APIs.
 
 ### Use local LLMs
 
-If you want to run LLMs locally (e.g., deepseek-r1), you can use Ollama with this app. To do so,
+If you want to run LLMs locally (e.g., deepseek-r1), you can use Ollama or llama.cpp with this app. Note that you need llama.cpp for RAG.
+
+#### llama.cpp
+
+1. Follow the instriction in [llama.cpp Quick start](https://github.com/ggml-org/llama.cpp/tree/master?tab=readme-ov-file#quick-start) to install llama.cpp.
+2. Launch a llama-server via `llama-server -hf <hugging-face model>`.
+
+#### Ollama
 
 1. [Download](https://ollama.ai/download) and install Ollama onto the available supported platforms (including Windows Subsystem for Linux)
 2. Fetch available LLM model via `ollama pull <name-of-model>`
@@ -33,4 +52,12 @@ If you want to call APIs (e.g., OpenAI models), you must store your API key in a
 
 ```bash
 export OPENAI_API_KEY="your-api-key"
+```
+
+## Running the app
+
+You can simply run the streamlit app
+
+```bash
+streamlit run app.py
 ```
--- grading_utils.py
+++ grading_utils.py
@@ -3,6 +3,27 @@ from grading_pipeline_similarity import GradingSystemSimilarity
 from grading_pipeline_llm import GradingSystemLLM
 from grading_pipeline_dummy import GradingSystemDummy
 
+PROBLEMS = [
+    {
+        "problem": "For a truss or a frame, each connection to the ground provides up to two force components and one moment depending on the type of support used. For the given structure, what are the correct reaction forces at the support A and D? A is a hinge support, and D is a roller support.",
+        "images": [
+            "../Updated Report and Collected Dataset_SBU/Assigned Questions and Collected Response/Cycle 1/Practice1 Problem.jpg",
+            "../Updated Report and Collected Dataset_SBU/Assigned Questions and Collected Response/Cycle 1/Practice1 Choices.jpg"
+            ]
+        ,
+        "choices": ["a", "b", "c", "d", "e"],
+        "answer": "e"
+    }
+]
+TEXT_PATH = "../samples/StaticsEngineeringMechanicsRCHibbelerbook12th.pdf"
+
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    if torch.backends.mps.is_available():
+        return "mps"
+    return "cpu"
+
 def get_grading_system(method: str):
     """
     Initialize grading system and grade assignment
@@ -15,11 +36,7 @@ def get_grading_system(method: str):
             coefficient = 0.95
         grading_system = GradingSystemDummy(coefficient=coefficient)
     elif method == "similarity":
-        device = "cpu"
-        if torch.cuda.is_available():
-            device = "cuda"
-        elif torch.backends.mps.is_available():
-            device = "mps"
+        device = get_device()
         grading_system = GradingSystemSimilarity(device=device)
 
     else:
