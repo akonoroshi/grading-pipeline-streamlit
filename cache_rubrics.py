1c1777f53e92e4eb140bf7cc7c8e28da972afbaa
akonoroshi <akr.oedo@gmail.com>
2025-08-18 10:58:17 -0700

    Refactoring caching rubrics

--- cache_rubrics.py
+++ cache_rubrics.py
@@ -1,35 +1,55 @@
-import pickle
-import os
-import re
 import tempfile
-import copy
-import json
+import os
+import pickle
+from typing import List
 from pdf2image import convert_from_path
+import jsonlines
+from pydantic import BaseModel, Field
+from utils import read_rubrics, format_rubrics
+from llm_utils import image_content, get_model
 from domain_information import PROBLEMS, TEXT_PATH
-from document_processor import RubricProcessor
 
-base_messsage = [{
-    "role": "system",
-    "content": """I have generic rubrics to evaluate student written rationales for their choice to any multiple-choice problems. Tailor them to the given problem by incorporating knowledge needed to solve it. 
-I will give you the problem, example rationale that received a full mark, rubrics, and seven images. The first image is the system diagram of the problem, the second image contains the choices, and the remaining images are textbook pages that may contain knowledge helpful to solve the problem."""
-}]
-processor = RubricProcessor()
-text = processor.process_document("./rubrics/domain-specific rubrics.docx")
-for problem_name ,problem in PROBLEMS.items():
-    pkl_path = os.path.join("problems", problem_name, "pages.pkl")
-    with open(pkl_path, 'rb') as f:
-        pages = pickle.load(f)
-    json_path = os.path.join("problems", problem_name, "description.json")
-    with open(json_path, "r") as f:
-        description = json.load(f)
-    
-    
+class Rubric(BaseModel):
+    name: str = Field(description="Name of the rubric.")
+    description: str = Field(description="What this rubrics evaluate in student rationales.")
+    high: str = Field(description="The characterisics of student rationales with a high score. This new description must contain domain knowledge from the textbook.")
+    medium: str = Field(description="The characterisics of student rationales with a medium score. This new description must contain domain knowledge from the textbook.")
+    low: str = Field(description="The characterisics of student rationales with a low score. This new description must contain domain knowledge from the textbook.")
+    very_low: str = Field(description="The characterisics of student rationales with a very low score. This new description must contain domain knowledge from the textbook.")
+
+class Rubrics(BaseModel):
+    rubrics: List[Rubric] = Field(description="List of rubrics for grading student rationales.")
+
+def get_user_content(problem: dict, page_images: list) -> List[dict]:
+    user_content = [{
+        "type": "text",
+        "text": f"""Please tailor the rubrics to evaluate student written rationales for their choice to the multiple-choice problem by incorporating knowledge needed for the problem. 
+I will give you the problem, rubrics, and seven images. The first image is the system diagram of the problem, the second image contains the choices, and the remaining images are textbook pages that may contain knowledge helpful to solve the problem.
+
+Problem: {problem["problem"]}"""}]
+    for img in problem["images"]:
+        user_content.append(image_content(img))
+    for img in page_images:
+        user_content.append(image_content(img))
+    return user_content
+
+def generate_rubrics(problem: dict,
+                     pages: List[int],
+                     model_name: str,
+                     textbook_path: str) -> List[Rubric]:
+    """
+    Generate rubrics for a given problem using the provided pages.
+    """
+
     page_images = []
-    modified_text = ""
+    llm = get_model(model_name).with_structured_output(Rubrics)
+    rubric_items = read_rubrics("./rubrics/generic_rubrics.jsonl")
+    formatted_rubrics = format_rubrics(rubric_items)
+    
     with tempfile.TemporaryDirectory() as path:
         for page in pages:
             page_images.extend(convert_from_path(
-                TEXT_PATH,
+                textbook_path,
                 thread_count=os.cpu_count() - 1,
                 output_folder=path,
                 dpi=300,
@@ -38,34 +58,36 @@ for problem_name ,problem in PROBLEMS.items():
                 last_page=page,
                 paths_only=True
             ))
-        user_content = processor.get_user_content(problem, page_images)
+        user_content = get_user_content(problem, page_images)
+     
+        user_content[0]["text"] += f"\n\nRubrics:\n{formatted_rubrics}"
+        messages = [{
+            "role": "user",
+            "content": user_content
+        }]
+        response = llm.invoke(messages)
 
-        for pattern in processor.section_patterns:
-            sections = re.split(pattern, text)
-            if len(sections) > 1:
-                for i, section in enumerate(sections):
-                    print("Section", section)
-                    if not section.strip():
-                        continue
-                    user_content_rubric = copy.deepcopy(user_content)
-                    user_content_rubric[0]["text"] += f"\nExample rationale: {description["rationale"]}\nRubric: {section}"
-                    messages = [{
-                        "role": "user",
-                        "content": user_content_rubric
-                    }]
-                    response = processor.llm.invoke(messages)
-                    print("Response:", response)
-                    modified_text += f"{i+1}. {response}\n"
-        """
-        user_content[0]["text"] += f"\nRubric: {text}"
-        messages = base_messsage + [{
-                        "role": "user",
-                        "content": user_content
-                    }]
-        response = processor.llm.invoke(messages)
-        print("Response:", response.rubrics)
-        modified_text = response.rubrics
-        """
+    return response.rubrics
 
-    with open(os.path.join("problems", problem_name, "rubrics.txt"), "w") as f:
-        f.write(modified_text)
+if __name__ == "__main__":
+    rubric_items = read_rubrics("rubrics/generic_rubrics.jsonl")
+    for problem_path, problem in PROBLEMS.items():
+        print(f"Generating rubrics for {problem_path}...")
+        with open(os.path.join("./problems", problem_path, "pages.pkl"), "rb") as f:
+            pages = pickle.load(f)
+        new_rubrics = generate_rubrics(problem, pages, "gpt-4.1-mini", TEXT_PATH)
+        assert len(new_rubrics) == len(rubric_items), "The number of generated rubrics does not match the number of generic rubrics."
+        for i, rubric in enumerate(new_rubrics):
+            rubric_items[i]["criteria"] = rubric.name + f" ({int(rubric_items[i]['points'])} Points)"
+            rubric_items[i]["description"] = rubric.description
+            for j in range(len(rubric_items[i]["labels"])):
+                if rubric_items[i]["labels"][j]["label"] == "High":
+                    rubric_items[i]["labels"][j]["description"] = rubric.high
+                elif rubric_items[i]["labels"][j]["label"] == "Medium":
+                    rubric_items[i]["labels"][j]["description"] = rubric.medium
+                elif rubric_items[i]["labels"][j]["label"] == "Low":
+                    rubric_items[i]["labels"][j]["description"] = rubric.low
+                elif rubric_items[i]["labels"][j]["label"] == "Very Low":
+                    rubric_items[i]["labels"][j]["description"] = rubric.very_low
+        with jsonlines.open(os.path.join("./problems", problem_path, "rubrics.jsonl"), 'w') as writer:
+            writer.write_all(rubric_items)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from typing import List
+import jsonlines
 import torch
 
 def get_device():
@@ -6,3 +8,38 @@ def get_device():
     if torch.backends.mps.is_available():
         return "mps"
     return "cpu"
+
+
+def read_rubrics(file_path: str) -> List[dict]:
+    """
+    Read rubrics from a JSON Lines file.
+    
+    Args:
+        file_path: Path to the JSON Lines file containing rubric items.
+        
+    Returns:
+        A list of dictionaries, each representing a rubric item.
+    """
+    with jsonlines.open(file_path, 'r') as reader:
+        return [item for item in reader]
+
+def format_rubrics(rubric_items: List[dict]) -> str:
+    """
+    Format the rubric items into a string for better readability.
+    
+    Args:
+        rubric_items: List of dictionaries containing rubric items.
+        
+    Returns:
+        A formatted string representation of the rubric items.
+    """
+    formatted_rubrics = []
+    for i, item in enumerate(rubric_items):
+        labels = []
+        for label in item['labels']:
+            labels.append(f"• {label["label"]} ({int(label["min"])}-{int(label["max"])} points): {label["description"]}")
+        labels = "\n".join(labels)
+        #formatted_rubrics.append(f"{item['criteria']}: {item['description']}\n{labels}")
+        formatted_rubrics.append(f"{i+1}. {item['criteria']}: {item['description']}\n{labels}")
+    #return formatted_rubrics
+    return "\n".join(formatted_rubrics)
